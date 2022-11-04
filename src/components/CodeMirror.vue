d756a21bc6cce7962e7fb1c72a26332b326ba9ed
yxzlwz <danny070601@aliyun.com>
2022-11-05 02:39:54 +0800

    [update] update code display

--- src/components/CodeMirror.vue
+++ src/components/CodeMirror.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref } from 'vue';
 import { Codemirror } from 'vue-codemirror';
-import 'codemirror/theme/panda-syntax.css';
 import { cpp } from '@codemirror/lang-cpp';
 import { python } from '@codemirror/lang-python';
+import { oneDark } from '@codemirror/theme-one-dark';
+import store from '@/store';
 const extensions = { python3: [python()], cpp: [cpp()], c: [cpp()] };
 
 const emit = defineEmits(['update:code']);
@@ -30,31 +31,21 @@ watch(_code, val => emit('update:code', val));
     :autofocus="true"
     :indent-with-tab="true"
     :tab-size="4"
-    :extensions="extensions[language]"
+    :extensions="
+      (extensions[language] ?? []).concat(
+        store.state.theme === 'dark' ? [oneDark] : []
+      )
+    "
   />
 </template>
 
-<style lang="scss">
-@font-face {
-  font-family: 'SourceCodePro';
-  src: url('https://cdn.jsdelivr.net/npm/source-code-pro@2.38.0/TTF/SourceCodePro-Regular.ttf')
-    format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
-
-.ͼ1.cm-editor.cm-focused {
+<style lang="scss" scoped>
+:deep(.ͼ1.cm-editor.cm-focused) {
   outline: none;
 }
 
-.ͼ1.cm-editor div,
-.ͼ1.cm-editor span {
+:deep(.ͼ1.cm-editor div),
+:deep(.ͼ1.cm-editor span) {
   font-family: 'SourceCodePro';
-}
-
-.ͼ2 .cm-gutters {
-  background-color: var(--n-color);
-  transition: color 0.3s var(--n-bezier), background-color 0.3s var(--n-bezier),
-    box-shadow 0.3s var(--n-bezier), border-color 0.3s var(--n-bezier);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import { store } from './store';
+import '@/assets/style.scss';
 
 const app = createApp(App);
 app.use(router).use(store);
